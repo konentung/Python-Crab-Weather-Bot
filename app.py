cd31801bce70d33f2049abaa2775a9bbf70c01f2
konentung <84655730+konentung@users.noreply.github.com>
2024-07-05 22:20:44 +0800

    feature-新增功能 1.縣市選擇的quick reply 2.抓取資料的縣市distinct

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ from linebot.v3.exceptions import (
 )
 from linebot.v3.webhooks import (
     MessageEvent,
-    TextMessageContent,
-    LocationMessageContent
+    TextMessageContent
 )
 from linebot.v3.messaging import (
     Configuration,
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
+    QuickReply,
+    QuickReplyItem,
+    PostbackAction,
     TextMessage
 )
 import os
@@ -27,10 +29,6 @@ app = Flask(__name__)
 
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
-clu_endpoint = os.getenv("CLU_ENDPOINT")
-clu_key = os.getenv("CLU_KEY")
-project_name = os.getenv("PROJECT_NAME")
-deployment_name = os.getenv("DEPLOYMENT_NAME")    
 
 line_handler = WebhookHandler(CHANNEL_SECRET)
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
@@ -50,52 +48,52 @@ def callback():
         abort(400)
     return 'OK'
 
-@line_handler.add(event=MessageEvent, message=LocationMessageContent)
-def handle_location_message(event):
-    client = ConversationAnalysisClient(clu_endpoint, AzureKeyCredential(clu_key))
-    with client:
-        address = event.message.address
-        result = client.analyze_conversation(
-            task={
-                "kind": "Conversation",
-                "analysisInput": {
-                    "conversationItem": {
-                        "participantId": "1",
-                        "id": "1",
-                        "modality": "text",
-                        "language": "zh-tw",
-                        "text": address
-                    },
-                    "isLoggingEnabled": False
-                },
-                "parameters": {
-                    "projectName": project_name,
-                    "deploymentName": deployment_name,
-                    "verbose": True
-                }
-            }
-        )
-    messages = []
-    entities = result['result']['prediction']['entities']
-    if len(entities) == 2 and entities[0]['category'] == 'city' and entities[1]['category'] == 'town':
-        messages.append(TextMessage(text=f"你傳送的位址資訊的城市:{result['result']['prediction']['entities'][0]['text']}"))
-        messages.append(TextMessage(text=f"你傳送的位址資訊的鄉鎮:{result['result']['prediction']['entities'][1]['text']}"))
+@line_handler.add(MessageEvent, message=TextMessageContent)
+def handle_messsage(event):
+    messages = event.message.text
+    if messages == "空氣品質":
+        data = import_data()
+        county_list = distinct_county(data)
+        items = []
+        for i in range(0, len(county_list)-9):
+            items.append(
+                QuickReplyItem(
+                    action=PostbackAction(
+                        label=county_list[i],
+                        data=county_list[i]
+                    )
+                )
+            )
+        quick_reply = QuickReply(items=items)
+        messages = [TextMessage(text="請選擇縣市", quick_reply=quick_reply)]
+        reply_message(event, messages)
     else:
-        messages.append(TextMessage(text="無法辨識你傳送的位址資訊"))
-
+        messages = [TextMessage(text=event.message.text)]
+        reply_message(event, messages)
+    
+def reply_message(event, messages):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
+        line_bot_api.reply_message(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
                 messages=messages
             )
         )
-        
-def read_weather_data():
+
+def import_data():
     url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate desc&format=CSV"
     url = url.replace(" ", "%20")
     data = pd.read_csv(url, encoding="utf-8")
+    data = pd.DataFrame(data)
+    return data
+
+def distinct_county(data):
+    county_list = list(data.county.unique())
+    return county_list
+
+def select_county(data, county):
+    data = data[data["county"] == county]
     return data
         
 if __name__ == "__main__":
